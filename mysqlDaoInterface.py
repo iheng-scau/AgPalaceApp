229900c70a60788be070ad5c63decb2094f7eae4
iheng <iheng_scau@hotmail.com>
2015-12-07 19:12:01 +0800

    null

--- mysqlDaoInterface.py
+++ mysqlDaoInterface.py
@@ -4,7 +4,7 @@ import sys
 import logging
 
 MYSQL_DB=sae.const.MYSQL_DB
-MySQL_USER=sae.const.MySQL_USER
+MYSQL_USER=sae.const.MYSQL_USER
 MYSQL_PASS=sae.const.MYSQL_PASS
 MYSQL_HOST=sae.const.MYSQL_HOST
 MYSQL_PORT=sae.const.MYSQL_PORT
@@ -13,7 +13,7 @@ MYSQL_HOST_S=sae.const.MYSQL_HOST_S
 class MySqlDaoInterface:
 
 	def  __init__(self):
-		self.conn=MYSQLdb.connect(host=MYSQL_HOST,user=MySQL_USER,password=MYSQL_PASS,port=MYSQL_PORT,db=MYSQL_DB,charset='utf-8')
+		self.conn=MYSQLdb.connect(host=MYSQL_HOST,user=MYSQL_USER,password=MYSQL_PASS,port=MYSQL_PORT,db=MYSQL_DB,charset='utf-8')
 
 	def testConn(self):
 		cursor=self.conn.cursor()
