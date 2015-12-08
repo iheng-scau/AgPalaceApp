47ff8eafe845794898fe37d213b6aab4bc7b090b
iheng-scau <iheng_scau@hotmail.com>
2015-12-08 23:25:00 +0800

    1

--- handlerInterface.py
+++ handlerInterface.py
@@ -47,7 +47,7 @@ class HandlerInterface:
 		elif content=='music':
 			#test_content="<xml>+/<ToUserName><![CDATA["+self.fromUser+"]]></ToUserName><FromUserName><![CDATA["+self.toUser+"]]></FromUserName><CreateTime>1449569225</CreateTime><MsgType><![CDATA[music]]></MsgType><Music><Title><![CDATA[title]]></Title><Description><![CDATA[description]]></Description><MusicUrl><![CDATA[music_url]]></MusicUrl><HQMusicUrl><![CDATA[HQ_MUSIC_Url]]></HQMusicUrl><ThumbMediaId><![CDATA[media_id]]></ThumbMediaId></Music></xml>"
 			return self.onMusic()
-		elif re.search(r'天气$',content).group()!=None:
+		elif re.search(r'天气$',content)!=None:
 			return slef.onWeather()
 		elif content=='testdb':
 			self.testDB()
