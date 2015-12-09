236faced0143c58f51ee26b6979aeb84cb96ba72
iheng <iheng_scau@hotmail.com>
2015-12-09 10:59:49 +0800

    1

--- handlerInterface.py
+++ handlerInterface.py
@@ -92,7 +92,7 @@ class HandlerInterface:
 		req=urllib2.Request(url)
 		res=urllib2.urlopen(req)
 		data=res.read()
-		xml=etree.fromstring(data.decode('utf-8'))
+		xml=etree.fromstring(data)
 		city=xml.find('city').text
 		status1=xml.find('status1').text
 		status2=xml.find('status2').text
